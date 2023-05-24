c91b77f9d2949d29efd91af443963551b8445da3
IngReyes822 <114837990+IngReyes822@users.noreply.github.com>
2023-05-24 17:52:04 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using PrimerTarea_Majo_Victor_23CV.Entities;
+using PrimerTarea_Majo_Victor_23CV.Services;
+using System;
+
+namespace PrimerTarea_Majo_Victor_23CV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClienteServices servicio = new ClienteServices();
+
+            Cliente cliente1 = servicio.Agregar();
+            servicio.ImprimirDatos(cliente1);
+
+            Console.WriteLine(cliente1);
+
+        }
+    }
+}
---
+++ Services/ClienteServices.cs
@@ -0,0 +1,54 @@
+﻿using PrimerTarea_Majo_Victor_23CV.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerTarea_Majo_Victor_23CV.Services
+{
+    public class ClienteServices
+    {
+
+       public  Cliente Agregar()
+        {
+            try
+            {
+                Cliente clienteCompra = new Cliente()
+                {
+                    Nombre = "Victor",
+                    Telefono = "9983304505",
+                    Correo = "victor.vu583@gmail.com"
+                   
+                };
+                return clienteCompra;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Sucedio un error" + ex.Message);
+            }
+        }
+
+        public void ImprimirDatos(Cliente request)
+        {
+            try
+            {
+                Console.WriteLine("El nombre es: " + request.Nombre);
+                Console.WriteLine("El apellido es: " + request.Telefono);
+                Console.WriteLine("El dia de ingreso  es: " + request.Correo);
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Succedio un error " + ex.Message);
+               
+            }
+        }
+        
+
+
+
+
+    }
+}
